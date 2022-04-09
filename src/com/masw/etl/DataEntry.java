bf435f2d79825e017feab6a5a5400d111f8ea53c
Nacho Ballesta <nachoballesta.99@gmail.com>
2022-04-09 19:35:59 +0200

    Ammends

--- src/com/masw/etl/DataEntry.java
+++ src/com/masw/etl/DataEntry.java
@@ -1,12 +1,7 @@
 package com.masw.etl;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@AllArgsConstructor
 public class DataEntry {
 
     private String title;
@@ -20,6 +15,63 @@ public class DataEntry {
     private String ratingRT;
 
     private List<Platform> platforms;
+
+    public DataEntry(String title, String year, String age, String ratingIdmb, String ratingRT, List<Platform> platforms) {
+        this.title = title;
+        this.year = year;
+        this.age = age;
+        this.ratingIdmb = ratingIdmb;
+        this.ratingRT = ratingRT;
+        this.platforms = platforms;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getRatingIdmb() {
+        return ratingIdmb;
+    }
+
+    public void setRatingIdmb(String ratingIdmb) {
+        this.ratingIdmb = ratingIdmb;
+    }
+
+    public String getRatingRT() {
+        return ratingRT;
+    }
+
+    public void setRatingRT(String ratingRT) {
+        this.ratingRT = ratingRT;
+    }
+
+    public List<Platform> getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(List<Platform> platforms) {
+        this.platforms = platforms;
+    }
 
 
     public enum Platform {
--- src/com/masw/etl/Main.java
+++ src/com/masw/etl/Main.java
@@ -1,8 +1,11 @@
 package com.masw.etl;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
